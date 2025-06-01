75baf79f45c637e4909928e280ad62b8777ce0d7
tara dalaei <91779735+taradalaei@users.noreply.github.com>
2025-06-01 18:42:48 +0330

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,17 @@
+CC = gcc
+CFLAGS = -Wall -lm
+TARGET = scheduler_test
+HDR = scheduler.h
+SRC = scheduler.c scheduler_test.c
+
+
+all: $(TARGET)
+
+$(TARGET): $(SRC)
+	$(CC) $(CFLAGS) $(SRC) -o $(TARGET)
+
+run: $(TARGET)
+	./$(TARGET)
+
+clean:
+	rm -f $(TARGET)
---
+++ scheduler.c
@@ -0,0 +1,18 @@
+#include "scheduler.h"
+
+// ---------------- Scheduling Algorithms ----------------
+
+// FCFS Scheduling
+Metrics fcfs_metrics(Process proc[], int n) {
+    //TODO
+}
+
+// SJF Scheduling (Non-preemptive)
+Metrics sjf_metrics(Process proc[], int n) {
+    //TODO
+}
+
+// Round Robin Scheduling (Revised)
+Metrics rr_metrics(Process proc[], int n, int timeQuantum) {
+    //TODO
+}
